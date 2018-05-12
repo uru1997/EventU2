8108e39ffc4271f902fe8b595f99a1105c085a6e
Juan Fer <juan_fer.narvaez@uao.edu.co>
2018-05-11 23:27:32 -0500

    recupera datos

--- Tvpro2/app/src/main/java/com/codigo/json/tvpro2/Tab1Fragment.java
+++ Tvpro2/app/src/main/java/com/codigo/json/tvpro2/Tab1Fragment.java
@@ -5,9 +5,11 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -71,7 +73,11 @@ public class Tab1Fragment extends Fragment {
         //Glide.with(this /*context*/).load(url).into(imagen);
 
         //dbEventos = FirebaseDatabase.getInstance().getReference().child("Evento");
-        dbEventos = FirebaseDatabase.getInstance().getReference().getRoot();
+        //dbEventos = FirebaseDatabase.getInstance().getReference().getRoot();
+        //String claveEvento = dbEventos.getKey();
+        //String id = dbEventos.getKey();
+        dbEventos = FirebaseDatabase.getInstance().getReference().child("Evento");
+        Log.d(TAG, "onCreateView: " + dbEventos.child("Evento"));
         mAdapter = new MyAdapter(listaDatos, getContext());
         mRecyclerView.setAdapter(mAdapter);
 
@@ -86,29 +92,29 @@ public class Tab1Fragment extends Fragment {
                 hora = dataSnapshot.child("Hora").getValue().toString();
                 url = dataSnapshot.child("url").getValue().toString();*/
                 listaDatos.removeAll(listaDatos);
-                for(DataSnapshot dataSnapshot1:dataSnapshot.getChildren()){
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
                     nombre = dataSnapshot1.child("Nombre").getValue().toString();
                     fecha = dataSnapshot1.child("Fecha").getValue().toString();
                     hora = dataSnapshot1.child("Hora").getValue().toString();
                     url = dataSnapshot1.child("url").getValue().toString();
                     tema = dataSnapshot1.child("Tema").getValue().toString();
                     lugar = dataSnapshot1.child("Lugar").getValue().toString();
-                    tipo = dataSnapshot1.child("Tipo_Evento").getValue().toString();
+                    tipo = dataSnapshot1.child("Tipo_evento").getValue().toString();
                     duracion = dataSnapshot1.child("Duracion").getValue().toString();
                     costo = dataSnapshot1.child("Costo").getValue().toString();
-                    listaDatos.add(new DatosEventos(nombre,fecha,hora,tema,lugar,tipo,duracion,costo, url));
+                    listaDatos.add(new DatosEventos(nombre, fecha, hora, tema, lugar, tipo, duracion, costo, url));
+                    }
+                    mAdapter.notifyDataSetChanged();
+                    //Glide.with(getContext()).load(url).into(imagen);
+                    //listaDatos.add(new DatosEventos(nombre,fecha,hora,url));
                 }
-                mAdapter.notifyDataSetChanged();
-                //Glide.with(getContext()).load(url).into(imagen);
-                //listaDatos.add(new DatosEventos(nombre,fecha,hora,url));
-            }
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
 
-            }
-        };
-        dbEventos.addValueEventListener(eventListener);
+                }
+            };
+            dbEventos.addValueEventListener(eventListener);
 
         return view;
     }
